30f0ed9a5f8db351a48612988adaa769c51a953e
avinashjay1 <Avinash.Veershetty@bristlecone.com>
2025-07-22 18:27:18 +0530

    Update content of config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-config = {'db': 'http://barnes.com/explore/app/listfaq.html', 'api_key': 'e76c0e17747e41c38913dcf5f1785509c0c0e38a'}
+CONFIG = {'project_id': 4, 'debug': True, 'owner': 'vais-team1'}
